f7c9331d422efda978a28301199aeb865058fd1f
Turbobot <turbobot@vercel.com>
2025-03-16 17:54:44 +0530

    feat(create-turbo): apply official-starter transform

--- packages/db/prisma/migrations/20250313165624_added_project_type/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ProjectType" AS ENUM ('NEXTJS', 'REACT', 'REACT_NATIVE');
-
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "type" "ProjectType" NOT NULL DEFAULT 'NEXTJS';
